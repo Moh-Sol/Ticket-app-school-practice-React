337dc4f3beebf149d6a16f3111263937237d8533
Mohammad-sloiman <mohammad.soliman@utb.ecutbildning.se>
2021-02-20 11:16:02 +0100

    Update erorr.js

--- src/components/erorr.js
+++ src/components/erorr.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Error() {
     return (
         <div>
-            <h1> Error sida kompnenet </h1>
+            <h1> Page Not Found </h1>
         </div>
     );
 }
